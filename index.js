a39b55e5c285647b5e597fec2238ca3a2bc5f6d7
sunny-selector-0188 <sonya_dumitru@yahoo.com>
2019-01-31 03:58:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
 //var newArray = [];
 function destructivelyAppendKitten(name) {
-  return array.push('Ralf')
+  array.push('Ralf');
+  return array;
 }
